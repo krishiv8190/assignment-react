1c0835057c4a1d3ff50aeefee265be98da1dfab8
Krishiv <avin.pandita32@gmail.com>
2024-05-07 22:08:15 +0530

    Fixed error message alignment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,51 +1,51 @@
 import PropTypes from "prop-types";
 
 export default function Button({
-  children,
-  icon: Icon,
-  styleName,
-  ...otherProps
+    children,
+    icon: Icon,
+    styleName,
+    ...otherProps
 }) {
-  let styles;
-  switch (styleName) {
-    case "primary":
-      styles = "bg-[#1C1C1C] hover:bg-[#4E4E4E] text-white font-manrope";
-      break;
-    case "secondary":
-      styles = "bg-[#F1F1F1] hover:bg-[#EAEAEA] text-black font-manrope";
-      break;
-    case "form-true":
-      styles =
-        "bg-[#1C1C1C] mt-9 w-7/12 hover:bg-[#4E4E4E] text-white font-manrope font-semibold text-lg";
-      break;
-    case "form-false":
-      styles =
-        "bg-[#C9C9C9] hover:bg-[#EAEAEA] mt-9 w-7/12 text-white font-manrope font-semibold cursor-not-allowed";
-      break;
-    default:
-      styles = "bg-[#1C1C1C] hover:bg-[#4E4E4E] text-white font-manrope";
-  }
+    let styles;
+    switch (styleName) {
+        case "primary":
+            styles = "bg-[#1C1C1C] hover:bg-[#4E4E4E] text-white font-manrope";
+            break;
+        case "secondary":
+            styles = "bg-[#F1F1F1] hover:bg-[#EAEAEA] text-black font-manrope";
+            break;
+        case "form-true":
+            styles =
+                "bg-[#1C1C1C] mt-9 w-7/12 hover:bg-[#4E4E4E] max-w-lg  text-white font-manrope font-semibold text-lg";
+            break;
+        case "form-false":
+            styles =
+                "bg-[#C9C9C9] hover:bg-[#EAEAEA] mt-9 w-7/12 max-w-lg  text-white font-manrope font-semibold cursor-not-allowed";
+            break;
+        default:
+            styles = "bg-[#1C1C1C] hover:bg-[#4E4E4E] text-white font-manrope";
+    }
 
-  return (
-    <button
-      className={`${styles} px-6 py-3 rounded-full gap-2 h-16 font-semibold text-lg ${
-        Icon ? "flex gap-3 items-center" : ""
-      }`}
-      {...otherProps}
-    >
-      {children}
-      <span>{Icon && <Icon />}</span>
-    </button>
-  );
+    return (
+        <button
+            className={`${styles} px-6 py-3 rounded-full gap-2 h-16 font-semibold text-lg ${
+                Icon ? "flex gap-3 items-center" : ""
+            }`}
+            {...otherProps}
+        >
+            {children}
+            <span>{Icon && <Icon />}</span>
+        </button>
+    );
 }
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired,
-  icon: PropTypes.elementType,
-  styleName: PropTypes.oneOf([
-    "primary",
-    "secondary",
-    "form-true",
-    "form-false",
-  ]),
+    children: PropTypes.node.isRequired,
+    icon: PropTypes.elementType,
+    styleName: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "form-true",
+        "form-false",
+    ]),
 };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,104 +5,106 @@ import PropTypes from "prop-types";
 import Caution from "../assets/caution.svg";
 
 export default function Form({ onRegistration }) {
-  const [formValues, setFormValues] = useState({
-    name: "",
-    email: "",
-  });
-
-  const [error, setError] = useState({
-    name: false,
-    email: false,
-  });
-
-  const isValidEmail = (email) => {
-    return email.trim() === "" || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  };
-
-  const formChangeHandler = (event) => {
-    setFormValues((prevValues) => {
-      return {
-        ...prevValues,
-        [event.target.name]: event.target.value,
-      };
-    });
-  };
-
-  const registrationSubmitHandler = (event) => {
-    event.preventDefault();
-
-    const nameValid = formValues.name.trim() !== "";
-    const emailValid = isValidEmail(formValues.email);
-
-    setError({
-      name: !nameValid,
-      email: !emailValid,
+    const [formValues, setFormValues] = useState({
+        name: "",
+        email: "",
     });
 
-    if (nameValid && emailValid) {
-      onRegistration(true);
-    } else {
-      onRegistration(false);
-    }
-  };
+    const [error, setError] = useState({
+        name: false,
+        email: false,
+    });
 
-  return (
-    <section className="mt-10 flex flex-col items-center">
-      <div className="space-y-4">
-        <h2 className="text-center font-grace font-normal text-[#2DA950] text-4xl">
-          Registration Form
-        </h2>
-        <p className="text-center font-semibold text-6xl font-manrope">
-          Start your success <br /> Journey here!
-        </p>
-      </div>
-      <form
-        onSubmit={registrationSubmitHandler}
-        className="flex flex-col gap-4 mt-8 w-1/2 items-center"
-        noValidate
-      >
-        <Input
-          id="name"
-          type="text"
-          placeholder="Enter your name"
-          name="name"
-          value={formValues.name}
-          onChange={formChangeHandler}
-          additionalClasses="m-3"
-        />
-        <Input
-          id="email"
-          type="email"
-          placeholder="Enter your email"
-          name="email"
-          value={formValues.email}
-          onChange={formChangeHandler}
-          isError={error.email}
-        />
-        {error.email && (
-          <div className="text-[#FF0808] -mb-4 mt-3 mr-32 text-base font-medium flex justify-start items-center gap-1 pl-4 font-manrope">
-            <img
-              src={Caution}
-              alt="caution"
-              className="w-5 h-5 mr-1 justify-center items-center flex"
-            />
-            Please enter a valid email address
-          </div>
-        )}
-        {formValues.name && formValues.email ? (
-          <Button type="submit" styleName="form-true" formnovalidate>
-            Submit
-          </Button>
-        ) : (
-          <Button type="button" styleName="form-false" formnovalidate>
-            Submit
-          </Button>
-        )}
-      </form>
-    </section>
-  );
+    const isValidEmail = (email) => {
+        return email.trim() === "" || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+
+    const formChangeHandler = (event) => {
+        setFormValues((prevValues) => {
+            return {
+                ...prevValues,
+                [event.target.name]: event.target.value,
+            };
+        });
+    };
+
+    const registrationSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const nameValid = formValues.name.trim() !== "";
+        const emailValid = isValidEmail(formValues.email);
+
+        setError({
+            name: !nameValid,
+            email: !emailValid,
+        });
+
+        if (nameValid && emailValid) {
+            onRegistration(true);
+        } else {
+            onRegistration(false);
+        }
+    };
+
+    return (
+        <section className="mt-10 flex flex-col items-center">
+            <div className="space-y-4">
+                <h2 className="text-center font-grace font-normal text-[#2DA950] text-4xl">
+                    Registration Form
+                </h2>
+                <p className="text-center font-semibold text-6xl font-manrope">
+                    Start your success <br /> Journey here!
+                </p>
+            </div>
+            <form
+                onSubmit={registrationSubmitHandler}
+                className="flex flex-col gap-4 mt-8 w-1/2 items-center"
+                noValidate
+            >
+                <Input
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name"
+                    name="name"
+                    value={formValues.name}
+                    onChange={formChangeHandler}
+                    additionalClasses="m-3"
+                />
+                <Input
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={formValues.email}
+                    onChange={formChangeHandler}
+                    isError={error.email}
+                />
+                {error.email && (
+                    <div className="flex justify-center items-center w-full h-full">
+                        <div className="text-[#FF0808] w-1/2 max-w-lg text-base font-medium flex justify-start items-center gap-1 pl-4 font-manrope">
+                            <img
+                                src={Caution}
+                                alt="caution"
+                                className="w-5 h-5 mr-1 justify-center items-center flex"
+                            />
+                            Please enter a valid email address
+                        </div>
+                    </div>
+                )}
+                {formValues.name && formValues.email ? (
+                    <Button type="submit" styleName="form-true" formnovalidate>
+                        Submit
+                    </Button>
+                ) : (
+                    <Button type="button" styleName="form-false" formnovalidate>
+                        Submit
+                    </Button>
+                )}
+            </form>
+        </section>
+    );
 }
 
 Form.propTypes = {
-  onRegistration: PropTypes.func.isRequired,
+    onRegistration: PropTypes.func.isRequired,
 };
